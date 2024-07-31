735a7d31357f5e3f5ef2d44881b78e66b5dc6e8e
Avinash GM <117427788+avinashgm08@users.noreply.github.com>
2024-07-31 12:18:46 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+ 
+
+<center>
+  <h1><i>Pet Shop</h1>
+  <p>Find your new best friend today!</i></p>
+</center>
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="aniview.php">Animals</a></li>
+    <li><a href="productv.php">Products</a></li>
+  <li><a href="login.php">Login</a></li>
+</ul>
+
+    <form action="logindb.php" method="POST">
+  <div class="container">
+    <h1>Admin Login</h1>
+    <hr>
+
+    <label for="email"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" id="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="pass" id="psw" required>
+
+    <hr>
+    <button type="submit" class="registerbtn" name="login">Login</button>
+
+  </div>
+ 
+</form>
+
+
+</body>
+</html>
